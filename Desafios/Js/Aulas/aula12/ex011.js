1baa70800975d9f040358564fe6d54be397ba58c
Joao Victor <135065236+JVCastroDev@users.noreply.github.com>
2024-05-28 18:27:08 -0300

    Fix

--- Desafios/Js/Aulas/aula12/ex011.js
+++
@@ -1,9 +0,0 @@
-var idade = 16
-if (idade < 16) {
-    console.log('Nao vota')
-
-}else if(idade < 18){
-        console.log('Voto Opcional')
-}else if(idade >= 18) {
-    console.log('Voto obrigatorio')
-}
